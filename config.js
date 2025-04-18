3d620237a8b6290750f6ebe454c5a5b4e3c2cd0d
Atrone <162317231+Vishu7772@users.noreply.github.com>
2025-04-18 08:05:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,16 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  ownerID: ["1187666470990266410", ""], 
+  mongodbUri : "mongodb+srv://atrone764:Adityaloser1@atrone.hvxe3ua.mongodb.net/?retryWrites=true&w=majority&appName=Atrone",
+  spotifyClientId : "df1ee64dff6e4747a4029416a133baaa",
+  spotifyClientSecret : "3205aac99ade40cf89ae95e0635022f9",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/obontop",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
